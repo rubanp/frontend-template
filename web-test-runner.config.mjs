2e22e139d8f11919728d694aed9fefb37cc5973f
Ruban Prakash <rubanprakash@hotmail.com>
2022-05-19 16:10:19 +0100

    add puppeteer options to web test runner

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,4 +1,15 @@
+import { puppeteerLauncher } from '@web/test-runner-puppeteer';
+
 export default {
   puppeteer: true,
-  watch: true,
+  browsers: [
+    puppeteerLauncher({
+      launchOptions: {
+        executablePath: '/Users/ruban/Code/frontend-template/node_modules/puppeteer/.local-chromium/mac-991974/chrome-mac/Chromium.app/Contents/MacOS/Chromium',
+        headless: true,
+        devtools: true,
+        args: ['--disable-gpu', '--no-sandbox'],
+      },
+    }),
+  ],
 };
