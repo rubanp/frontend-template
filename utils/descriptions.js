3bcaafa962e7b6a68e32c87722706ade26facc96
Ruban Prakash <rubanprakash@hotmail.com>
2022-06-04 17:00:08 +0100

    add comments to organise commit type descripts

--- utils/descriptions.js
+++ utils/descriptions.js
@@ -1,4 +1,5 @@
 const descriptions = {
+  // API
   api: 'Refactoring that affects more than one part of the API',
   route: 'Add or update a route',
   cont: 'Add or update a controller',
@@ -6,26 +7,30 @@ const descriptions = {
   db: 'Add or update a CRUD operation',
   sub: 'Add or update a subscriber',
   job: 'Add or update a job',
+  // Views
   view: 'Refactoring that affects more than one part of the view',
   page: 'Add or update a page',
   reg: 'Add or update a region component',
   comp: 'Add or update a composite component',
   elem: 'Add or update an element component',
   style: 'Add or update global styles or design tokens',
-  meta: 'Update metadata like package.json and config files',
   asset: 'Add or update assets',
+  // Chores
+  meta: 'Update metadata like package.json or config files',
   build: 'Changes that affect the build system',
+  docs: 'Documentation only change',
+  test: 'Add or update tests',
+  temp: 'Add or change template file',
+  util: 'Add or change a utils file',
+  info: 'Add or update license, contributors or changelog',
+  // Dependencies
   bump: 'Upgrade a dependency',
   dip: 'Downgrade a dependency',
   add: 'Add a dependency',
   rm: 'Remove a dependency',
   pin: 'Pin dependency to specific version',
-  docs: 'Documentation only change',
-  test: 'Add or update tests',
-  temp: 'Add or change template file',
-  util: 'Add or change a utils file',
+  // Miscellaneous
   init: 'First commit of boilerplate code',
-  info: 'Add or update license, contributors and changelog',
   exp: 'Experiment with new code, then rebase or reset',
 };
 
