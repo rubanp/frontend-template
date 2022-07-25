95eb166a9e269d4de9a12a40c4575c23c24f4a1b
Ruban Prakash <rubanprakash@hotmail.com>
2022-07-25 15:09:20 +0100

    fix errors in commit save script

--- utils/save.js
+++ utils/save.js
@@ -8,28 +8,28 @@ fs.readFile('./templates/new.commit', (err, data) => {
   if (err) {
     console.error(err);
   } else {
-    console.log(boxen(data.toString('utf8'), {
+    console.log(boxen(data.toString('utf8').replace(/^\s+|\s+$/g, ''), {
       title: 'commit intentions', titleAlignment: 'center', borderColor: 'blue', padding: 1,
     }));
   }
 });
 
-let editTemplate = false;
-
 // Ask if user would like to edit the intentions
 const { Toggle } = Enquirer;
 setTimeout(async () => {
-  editTemplate = await new Toggle({
+  const editTemplate = await new Toggle({
     message: 'Would you like to edit your commit intentions to better reflect the changes you made?',
     enabled: 'Yes',
     disabled: 'No',
   }).run().then((answer) => answer).catch((err) => console.error(err));
+  if(editTemplate === undefined) {
+    process.exit(1);
+  }
+  if(editTemplate === true) {
+    childProcess.spawn('nvim', ['./templates/new.commit'], {
+      stdio: 'inherit',
+    });
+  }
+  exec('git commit -m "$(cat "./templates/new.commit")"');
+  exec('rm ./templates/new.commit');
 }, 100);
-
-if (editTemplate) {
-  childProcess.spawn('nvim', ['./templates/new.commit'], {
-    stdio: 'inherit',
-  });
-}
-
-exec('git commit --allow-empty -m "$(cat "./templates/new.commit")"');
